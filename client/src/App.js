653d5f9dd5e25f08824d714fa864bede95b5c537
julie <hlee748@gmail.com>
2021-11-12 13:12:54 +0900

    let start

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Landing from './pages/Landing';
+import Map from './pages/Map';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App () {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path='/'>
+          <Landing/>
+        </Route>
+        <Route exact path='/map'>
+          <Map />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;

c1ca511e474a2726169770f296c2e686cbd245af
HyeRyun Choi <hyeryunchoi@HyeRyunui-MacBookPro.local>
2021-11-12 17:31:23 +0900

    유저디비생성

---
+++ server/migrations/20211112054944-create-users.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nickName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      birth: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
